7b76b98cd70505273e7fe79ea7902a0ee9c8e4bc
Jaroslav Imrich <jaroslav.imrich@gmail.com>
2015-01-15 00:18:56 +0100

    Updated year in copyright

--- src/Pkcs11Admin/Properties/AssemblyInfo.cs
+++ src/Pkcs11Admin/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Pkcs11Admin")]
-[assembly: AssemblyCopyright("Copyright (c) 2014 Jaroslav Imrich")]
+[assembly: AssemblyCopyright("Copyright (c) 2014-2015 Jaroslav Imrich")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
