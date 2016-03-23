349d8d7de9a7ddc130a4cb307e6956be1c974740
Jaroslav Imrich <jaroslav.imrich@gmail.com>
2016-03-24 00:05:25 +0100

    Added BouncyCastle license to AboutDialog

--- src/Pkcs11Admin.WinForms/Dialogs/AboutDialog.cs
+++ src/Pkcs11Admin.WinForms/Dialogs/AboutDialog.cs
@@ -139,6 +139,17 @@ namespace Net.Pkcs11Admin.WinForms.Dialogs
             RichTextBoxLicenses.SelectionFont = new System.Drawing.Font(fontFamily, fontSize, FontStyle.Regular);
             RichTextBoxLicenses.AppendText(@"Asn1Net.Forms.TreeView is available under the terms of the GNU Affero General Public License version 3 as published by the Free Software Foundation. Please see < http://www.gnu.org/licenses/agpl-3.0.html> for more details." + Environment.NewLine + Environment.NewLine);
 
+            RichTextBoxLicenses.SelectionFont = new System.Drawing.Font(fontFamily, fontSize * 2, FontStyle.Bold);
+            RichTextBoxLicenses.AppendText(@"The Bouncy Castle C# API" + Environment.NewLine);
+            RichTextBoxLicenses.SelectionFont = new System.Drawing.Font(fontFamily, fontSize, FontStyle.Bold);
+            RichTextBoxLicenses.AppendText(@"Copyright (c) 2000 - 2015 The Legion of the Bouncy Castle Inc. (http://www.bouncycastle.org)" + Environment.NewLine + Environment.NewLine);
+            RichTextBoxLicenses.SelectionFont = new System.Drawing.Font(fontFamily, fontSize, FontStyle.Regular);
+            RichTextBoxLicenses.AppendText(@"Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the ""Software""), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sub license, and / or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:" + Environment.NewLine + Environment.NewLine);
+            RichTextBoxLicenses.SelectionFont = new System.Drawing.Font(fontFamily, fontSize, FontStyle.Regular);
+            RichTextBoxLicenses.AppendText(@"The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software." + Environment.NewLine + Environment.NewLine);
+            RichTextBoxLicenses.SelectionFont = new System.Drawing.Font(fontFamily, fontSize, FontStyle.Regular);
+            RichTextBoxLicenses.AppendText(@"THE SOFTWARE IS PROVIDED ""AS IS"", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE." + Environment.NewLine + Environment.NewLine);
+
             RichTextBoxLicenses.SelectionStart = 0;
             RichTextBoxLicenses.ScrollToCaret();
         }
