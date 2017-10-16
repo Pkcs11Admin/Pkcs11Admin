b143ee24d1a6a07a08e11139d6efe3e62c79eb14
Jaroslav Imrich <jimrich@jimrich.sk>
2017-10-16 20:54:26 +0200

    Build script updated for VS2017

--- build/portable/build_portable_release.bat
+++ build/portable/build_portable_release.bat
@@ -1,60 +1,75 @@
-setlocal
+@setlocal
 
-@rem Initialize build environment of Visual Studio 2015
-call "c:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\Tools\vsvars32.bat"
+@rem Initialize Visual Studio build environment:
+@rem - Visual Studio 2017 Community/Professional/Enterprise is the preferred option
+@rem - Visual Studio 2015 is the fallback option (which might or might not work)
+@set tools=
+@set tmptools="c:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\Tools\vsvars32.bat"
+@if exist %tmptools% set tools=%tmptools%
+@set tmptools="c:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\Tools\VsMSBuildCmd.bat"
+@if exist %tmptools% set tools=%tmptools%
+@set tmptools="c:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\Common7\Tools\VsMSBuildCmd.bat"
+@if exist %tmptools% set tools=%tmptools%
+@set tmptools="c:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\Common7\Tools\VsMSBuildCmd.bat"
+@if exist %tmptools% set tools=%tmptools%
+@if not defined tools goto :error
+call %tools%
+@echo on
 
 @rem Build for x86 platform
-msbuild ..\..\src\Pkcs11Admin.sln /p:Configuration=Release /p:Platform=x86 /target:Clean || exit /b 1
-msbuild ..\..\src\Pkcs11Admin.sln /p:Configuration=Release /p:Platform=x86 /target:Build || exit /b 1
+msbuild ..\..\src\Pkcs11Admin.sln /p:Configuration=Release /p:Platform=x86 /target:Clean || goto :error
+msbuild ..\..\src\Pkcs11Admin.sln /p:Configuration=Release /p:Platform=x86 /target:Build || goto :error
 
 @rem Build for x64 platform
-msbuild ..\..\src\Pkcs11Admin.sln /p:Configuration=Release /p:Platform=x64 /target:Clean || exit /b 1
-msbuild ..\..\src\Pkcs11Admin.sln /p:Configuration=Release /p:Platform=x64 /target:Build || exit /b 1
+msbuild ..\..\src\Pkcs11Admin.sln /p:Configuration=Release /p:Platform=x64 /target:Clean || goto :error
+msbuild ..\..\src\Pkcs11Admin.sln /p:Configuration=Release /p:Platform=x64 /target:Build || goto :error
 
 @rem Build for AnyCPU platform
-msbuild ..\..\src\Pkcs11Admin.sln /p:Configuration=Release /p:Platform=AnyCPU /target:Clean || exit /b 1
-msbuild ..\..\src\Pkcs11Admin.sln /p:Configuration=Release /p:Platform=AnyCPU /target:Build || exit /b 1
-
-endlocal
-
-setlocal
+msbuild ..\..\src\Pkcs11Admin.sln /p:Configuration=Release /p:Platform=AnyCPU /target:Clean || goto :error
+msbuild ..\..\src\Pkcs11Admin.sln /p:Configuration=Release /p:Platform=AnyCPU /target:Build || goto :error
 
 @rem Prepare output directory
 set OUTDIR=Pkcs11Admin-Release
 rmdir /S /Q %OUTDIR%
-mkdir %OUTDIR% || exit /b 1
-mkdir %OUTDIR%\license || exit /b 1
-mkdir %OUTDIR%\pkcs11-logger || exit /b 1
+mkdir %OUTDIR% || goto :error
+mkdir %OUTDIR%\license || goto :error
+mkdir %OUTDIR%\pkcs11-logger || goto :error
 
 @rem Copy binaries to the output directory
-copy ..\..\src\Pkcs11Admin.WinForms\bin\AnyCPU\Release\Asn1Net.Forms.TreeView.dll %OUTDIR% || exit /b 1
-copy ..\..\src\Pkcs11Admin.WinForms\bin\AnyCPU\Release\Asn1Net.Reader.dll %OUTDIR% || exit /b 1
-copy ..\..\src\Pkcs11Admin.WinForms\bin\AnyCPU\Release\Be.Windows.Forms.HexBox.dll %OUTDIR% || exit /b 1
-copy ..\..\src\Pkcs11Admin.WinForms\bin\AnyCPU\Release\Pkcs11Admin.dll %OUTDIR% || exit /b 1
-copy ..\..\src\Pkcs11Admin.WinForms\bin\AnyCPU\Release\Pkcs11Interop.dll %OUTDIR% || exit /b 1
-copy ..\..\src\Pkcs11Admin.WinForms\bin\x86\Release\Pkcs11Admin.WinForms.exe %OUTDIR%\Pkcs11Admin-x86.exe || exit /b 1
-copy ..\..\src\Pkcs11Admin.WinForms\bin\x86\Release\Pkcs11Admin.WinForms.exe.config %OUTDIR%\Pkcs11Admin-x86.exe.config || exit /b 1
-copy ..\..\src\Pkcs11Admin.WinForms\bin\x64\Release\Pkcs11Admin.WinForms.exe %OUTDIR%\Pkcs11Admin-x64.exe || exit /b 1
-copy ..\..\src\Pkcs11Admin.WinForms\bin\x64\Release\Pkcs11Admin.WinForms.exe.config %OUTDIR%\Pkcs11Admin-x64.exe.config || exit /b 1
-copy ..\..\src\lib\pkcs11-logger\pkcs11-logger-*.* %OUTDIR%\pkcs11-logger\ || exit /b 1
-copy ..\..\src\Pkcs11Admin.WinForms\bin\AnyCPU\Release\crypto.dll %OUTDIR% || exit /b 1
+copy ..\..\src\Pkcs11Admin.WinForms\bin\AnyCPU\Release\Asn1Net.Forms.TreeView.dll %OUTDIR% || goto :error
+copy ..\..\src\Pkcs11Admin.WinForms\bin\AnyCPU\Release\Asn1Net.Reader.dll %OUTDIR% || goto :error
+copy ..\..\src\Pkcs11Admin.WinForms\bin\AnyCPU\Release\Be.Windows.Forms.HexBox.dll %OUTDIR% || goto :error
+copy ..\..\src\Pkcs11Admin.WinForms\bin\AnyCPU\Release\Pkcs11Admin.dll %OUTDIR% || goto :error
+copy ..\..\src\Pkcs11Admin.WinForms\bin\AnyCPU\Release\Pkcs11Interop.dll %OUTDIR% || goto :error
+copy ..\..\src\Pkcs11Admin.WinForms\bin\x86\Release\Pkcs11Admin.WinForms.exe %OUTDIR%\Pkcs11Admin-x86.exe || goto :error
+copy ..\..\src\Pkcs11Admin.WinForms\bin\x86\Release\Pkcs11Admin.WinForms.exe.config %OUTDIR%\Pkcs11Admin-x86.exe.config || goto :error
+copy ..\..\src\Pkcs11Admin.WinForms\bin\x64\Release\Pkcs11Admin.WinForms.exe %OUTDIR%\Pkcs11Admin-x64.exe || goto :error
+copy ..\..\src\Pkcs11Admin.WinForms\bin\x64\Release\Pkcs11Admin.WinForms.exe.config %OUTDIR%\Pkcs11Admin-x64.exe.config || goto :error
+copy ..\..\src\lib\pkcs11-logger\pkcs11-logger-*.* %OUTDIR%\pkcs11-logger\ || goto :error
+copy ..\..\src\Pkcs11Admin.WinForms\bin\AnyCPU\Release\BouncyCastle.Crypto.dll %OUTDIR% || goto :error
 
 @rem Copy scripts to the output directory
-copy Pkcs11Admin-x86.sh %OUTDIR%\Pkcs11Admin-x86.sh || exit /b 1
-copy Pkcs11Admin-x64.sh %OUTDIR%\Pkcs11Admin-x64.sh || exit /b 1
+copy Pkcs11Admin-x86.sh %OUTDIR%\Pkcs11Admin-x86.sh || goto :error
+copy Pkcs11Admin-x64.sh %OUTDIR%\Pkcs11Admin-x64.sh || goto :error
 
 @rem Copy licenses to the output directory
-copy ..\..\src\lib\Asn1Net.Forms.TreeView\license.txt %OUTDIR%\license\Asn1Net.Forms.TreeView.txt || exit /b 1
-copy ..\..\src\lib\Asn1Net.Reader\license.txt %OUTDIR%\license\Asn1Net.Reader.txt || exit /b 1
-copy ..\..\src\lib\Be.Windows.Forms.HexBox\LICENSE.txt %OUTDIR%\license\Be.Windows.Forms.HexBox.txt || exit /b 1
-copy ..\..\src\LICENSE.txt %OUTDIR%\license\Pkcs11Admin.txt || exit /b 1
-copy ..\..\src\lib\Pkcs11Interop\LICENSE.txt %OUTDIR%\license\Pkcs11Interop.txt || exit /b 1
-copy ..\..\src\lib\Pkcs11Interop\NOTICE.txt %OUTDIR%\license\Pkcs11Interop-notice.txt || exit /b 1
-copy ..\..\src\lib\pkcs11-logger\license.txt %OUTDIR%\license\pkcs11-logger.txt || exit /b 1
-copy ..\..\src\lib\pkcs11-logger\NOTICE.txt %OUTDIR%\license\pkcs11-logger-notice.txt || exit /b 1
-copy ..\..\src\lib\Portable.BouncyCastle\license.txt %OUTDIR%\license\crypto.txt || exit /b 1
+copy ..\..\src\lib\Asn1Net.Forms.TreeView\license.txt %OUTDIR%\license\Asn1Net.Forms.TreeView.txt || goto :error
+copy ..\..\src\lib\Asn1Net.Reader\license.txt %OUTDIR%\license\Asn1Net.Reader.txt || goto :error
+copy ..\..\src\lib\Be.Windows.Forms.HexBox\LICENSE.txt %OUTDIR%\license\Be.Windows.Forms.HexBox.txt || goto :error
+copy ..\..\src\LICENSE.txt %OUTDIR%\license\Pkcs11Admin.txt || goto :error
+copy ..\..\src\lib\Pkcs11Interop\LICENSE.txt %OUTDIR%\license\Pkcs11Interop.txt || goto :error
+copy ..\..\src\lib\Pkcs11Interop\NOTICE.txt %OUTDIR%\license\Pkcs11Interop-notice.txt || goto :error
+copy ..\..\src\lib\pkcs11-logger\license.txt %OUTDIR%\license\pkcs11-logger.txt || goto :error
+copy ..\..\src\lib\pkcs11-logger\NOTICE.txt %OUTDIR%\license\pkcs11-logger-notice.txt || goto :error
+copy ..\..\src\lib\Portable.BouncyCastle\license.txt %OUTDIR%\license\BouncyCastle.Crypto.txt || goto :error
 
-endlocal
+@endlocal
 
 @echo *** BUILD SUCCESSFUL ***
+@endlocal
 @exit /b 0
+
+:error
+@echo *** BUILD FAILED ***
+@endlocal
+@exit /b 1
