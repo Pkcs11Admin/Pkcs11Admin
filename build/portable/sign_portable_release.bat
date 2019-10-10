a3a2a01744224c28392055fc98f767722ff5d124
Jaroslav Imrich <jimrich@jimrich.sk>
2019-10-10 22:09:02 +0200

    Update sign script

--- build/portable/sign_portable_release.bat
+++ build/portable/sign_portable_release.bat
@@ -3,19 +3,13 @@
 set SIGNTOOL="C:\Program Files (x86)\Microsoft SDKs\ClickOnce\SignTool\signtool.exe"
 
 @rem Define signing options
-set CERTHASH=d5de31ea974f5ea8581d633eeffa8f3ea0d479bb
+set CERTHASH=ef1bfeaa474bb078923831bf7732186673a5b5c9
 set TSAURL=http://time.certum.pl/
 set LIBNAME=Pkcs11Admin
 set LIBURL=https://www.pkcs11admin.net/
 
-@rem Sign assemblies using SHA1withRSA algorithm
-%SIGNTOOL% sign /sha1 %CERTHASH% /fd sha1 /tr %TSAURL% /td sha1 /d %LIBNAME% /du %LIBURL% ^
-Pkcs11Admin-Release\Pkcs11Admin.dll ^
-Pkcs11Admin-Release\Pkcs11Admin-x86.exe ^
-Pkcs11Admin-Release\Pkcs11Admin-x64.exe || goto :error
-
-@rem Sign assemblies using SHA256withRSA algorithm
-%SIGNTOOL% sign /sha1 %CERTHASH% /as /fd sha256 /tr %TSAURL% /td sha256 /d %LIBNAME% /du %LIBURL% ^
+@rem Sign assemblies
+%SIGNTOOL% sign /sha1 %CERTHASH% /fd sha256 /tr %TSAURL% /td sha256 /d %LIBNAME% /du %LIBURL% ^
 Pkcs11Admin-Release\Pkcs11Admin.dll ^
 Pkcs11Admin-Release\Pkcs11Admin-x86.exe ^
 Pkcs11Admin-Release\Pkcs11Admin-x64.exe || goto :error
